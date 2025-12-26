258f412973576780c04e1280d87b934d1dbf01b2
andrewle9510 <andrewle9510@gmail.com>
2025-12-26 11:02:02 +0700

    ! update

--- Assets/Scripts/TestDash.cs
+++ Assets/Scripts/TestDash.cs
@@ -3,7 +3,10 @@ using MoreMountains.CorgiEngine;
 
 public class TestDash : CharacterDash
 {
+    [Header("Extras")]
     [SerializeField] private CorgiController _overridedController;
+    [SerializeField] private Character _characterNavigation;
+
 
     protected override void Initialization()
     {
@@ -11,6 +14,16 @@ public class TestDash : CharacterDash
         if (_overridedController != null)
         {
             _controller = _overridedController;
+        }
+    }
+
+    private void Update()
+    {
+        if (_characterNavigation)
+        {
+            _character.IsFacingRight = _characterNavigation.IsFacingRight;
+
+            Debug.Log(_character.IsFacingRight);
         }
     }
 }
