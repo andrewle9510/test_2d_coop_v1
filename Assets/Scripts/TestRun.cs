45b81d0838bba5b0f9732c831b95ad0d443a75aa
andrewle9510 <andrewle9510@gmail.com>
2025-12-26 11:09:35 +0700

    update

---
+++ Assets/Scripts/TestRun.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using MoreMountains.CorgiEngine;
+
+public class TestRun : CharacterRun
+{
+    [SerializeField] private CorgiController _overridedController;
+
+    protected override void Initialization()
+    {
+        base.Initialization();
+        if (_overridedController != null)
+        {
+            _controller = _overridedController;
+        }
+    }
+}
